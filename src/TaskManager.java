92ef6439b409a748ecc4b05d188d4361fd70d8c8
comilao <vincentm1991@gmail.com>
2015-03-06 13:55:36 +0800

    added the final ints for different elements in String[]

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
 
 public class TaskManager {
+    private static final int COMMAND_TYPE = 0;
+    private static final int ID = 1;
+    private static final int NAME = 2;
+    private static final int DATEFROM = 3;
+    private static final int DATETO = 4;
+    private static final int DEADLINE = 5;
+    private static final int LOCATION = 6;
+    private static final int DEATAIL = 7;
+    
+    
+    //cmd,id,name,datefrom,dateto,deadline,location,detail
+    
 	private ArrayList<Task> _tasks;
     
 	public TaskManager() {
-		
+		this._tasks = new ArrayList<Task>();
 	}
 	
-	public String[] processTaskCommand(String[] command) {
-		return null;
+	public ArrayList<Task> processTM(String[] input) {
+		
 	}
 }
