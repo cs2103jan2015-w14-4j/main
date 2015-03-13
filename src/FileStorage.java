4950eee7e79685edd5e9a8fa3173ce15c7353db0
weiquan <low.weiquan@gmail.com>
2015-03-13 15:34:18 +0800

    refactored: FileStorage

--- src/FileStorage.java
+++ src/FileStorage.java
@@ -2,29 +2,40 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+
 import java.text.Format;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
 public class FileStorage {
 	
-	private static final int COMMAND_TYPE = 0;
-	private static final int TASK_ID = 1;
-	private static final int TASK_NAME = 2;
-	private static final int DATE_FROM = 3;
-	private static final int DATE_TO = 4;
-	private static final int DEADLINE = 5;
-	private static final int LOCATION = 6;
-	private static final int DETAILS = 7;
-	private static final int PRIORITY = 8;
+	//The indexes for how a task object is stored in a ArrayList<Task>
+	private static final int COMMAND_TYPE_INDEX = 0;
+	private static final int TASK_ID_INDEX = 1;
+	private static final int TASK_NAME_INDEX = 2;
+	private static final int DATE_FROM_INDEX = 3;
+	private static final int DATE_TO_INDEX = 4;
+	private static final int DEADLINE_INDEX = 5;
+	private static final int LOCATION_INDEX = 6;
+	private static final int DETAILS_INDEX = 7;
+	private static final int PRIORITY_INDEX = 8;
 	
-	private static final String EMPTY_INPUT = "";
+	private static final String ERROR_EXCEPTION = "Exception caught";
+	
+	private static final String DEFAULT_FILENAME = "default.txt";
+	private static final String EMPTY_INPUT = "null";
 		
 	private static File textFile;
     
+	/**
+	 * Constructor for FileStorage object, this will store the text file name
+	 * in a global variable textFile for reference.
+	 * If the file does not exist it will be created.
+	 */
 	public FileStorage(String fileName) {
     	
 		textFile = new File(fileName);
@@ -36,11 +47,9 @@ public class FileStorage {
             
             }
 
-         } 
-         
-		catch(Exception e) {
+         }catch(Exception e) {
              
-        	 System.out.println("exception occurred"); 
+        	 System.out.println(ERROR_EXCEPTION); 
          
          }
 		
@@ -48,11 +57,11 @@ public class FileStorage {
 	
     public FileStorage() {
     	
-		textFile = new File("default.txt");
+		textFile = new File(DEFAULT_FILENAME);
     	
 	} 
 	
-    // takes the ||blah|| all of it at the end and store arraylist task
+    //stores each task as a string delimited by ,
     public void writeToFile(ArrayList<Task> taskList) {
     	
     	try {
@@ -60,49 +69,137 @@ public class FileStorage {
     	textFile.delete();
 		textFile.createNewFile();
 		BufferedWriter bw = new BufferedWriter(new FileWriter(textFile));
-		String newLine="\n";
-        for (int i = 0; i < taskList.size(); i++) {
+		for (int i = 0; i < taskList.size(); i++) {
             
         	Task tempTask = taskList.get(i);
-        	String[] taskArray = taskToStrArray(tempTask);
+        	String[] taskArray = taskToStringArray(tempTask);
         	for(int j = 0; j < taskArray.length; j++) {
-        	
-        	    bw.write(taskArray[j]);
-        	
+        		
+        		
+        		
+        		if ( j != taskArray.length-1) {
+        			
+        			taskArray[j] += (",");
+        			bw.write(taskArray[j]);
+        			
+        		}
+        		
+        		else {
+        			
+        			bw.write(taskArray[j]);
+        			
+        		}
+        		
         	}
-        	bw.write(newLine);
+        	
+        	bw.newLine();
         }
 
         bw.close();
     	
     	}catch(Exception e) {
     		
-    		System.out.println("Exception occurred");
+    		System.out.println(ERROR_EXCEPTION);
     		
     	 }
         
 	}
     
     
-    //no priority yet
-    private String[] taskToStrArray(Task tempTask) {
+    //String array is size 8 as priority is not included yet
+    private String[] taskToStringArray(Task tempTask) {
     	
     	String[] strArr = new String[8];
-    	
+
+    	//NO ERROR CATCHING FOR NULL ITEM
     	strArr[0] = IntegerToString(tempTask.getTID());
     	strArr[1] =	tempTask.getTaskName();
-    	strArr[2] =	dateToString(tempTask.getDateFrom());	
-    	strArr[3] = dateToString(tempTask.getDateTo());
-        strArr[4] = dateToString(tempTask.getDeadline());
-    	strArr[5] = tempTask.getLocation();
-    	strArr[6] = tempTask.getDetails();
-    	strArr[7] = IntegerToString(tempTask.getPriority());
+    	
+    	Date tempDate = tempTask.getDateFrom();
+    	
+    	if(tempDate != null) {
+        	
+    		strArr[2] =	dateToString(tempDate);	
+    	
+    	}
+    	
+    	else {
+    		
+    		strArr[2] = null;
+    		
+    	}
+    	
+    	tempDate = tempTask.getDateTo();
+    	
+    	if(tempDate != null) {
+    		
+    		strArr[3] = dateToString(tempDate);
+    	
+    	}
+    	
+    	else {
+    		
+    		strArr[3] = null;
+    		
+    	}
+    	
+    	tempDate = tempTask.getDeadline();
+    	
+    	if(tempDate != null) {
+    		
+    		strArr[4] = dateToString(tempDate);
+    	
+    	}
+    	
+    	else {
+    		
+    	    strArr[4] = null;
+    		
+    	}
+    	
+    	if(tempTask.getLocation() != null) {
+    	
+    		strArr[5] = tempTask.getLocation();
+    	
+    	}
+    	
+    	else {
+    		
+    		strArr[5] = null;
+    		
+    	}
+    	
+    	if(tempTask.getDetails() != null) {
+    	
+    		strArr[6] = tempTask.getDetails();
+    	
+    	}
+    	
+    	else {
+    		
+    		strArr[6] = null;
+    		
+    	}
+    	
+    	int tempInt = tempTask.getPriority();
+    	
+    	if(IntegerToString(tempInt) != null) {
+    	
+    		strArr[7] = IntegerToString(tempInt);
+    	
+    	}
+    	
+    	else {
+    		
+    		strArr[7] = null;
+    		
+    	}
     	
     	return strArr;
     	
     }
     
-    private String IntegerToString(int id ) {
+    private String IntegerToString(int id) {
     	
     	return Integer.toString(id);
     	
@@ -110,16 +207,19 @@ public class FileStorage {
     
     private String dateToString(Date date) {
     	
-    	Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    	Format formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
     	String dateString = formatter.format(date);
+    	
     	return dateString;
+    
     }
     
-    //return add,.. same as parse format;
+    /**
+     * extracts each line from specified file as String array
+     * and pass it as parameter to logic via processAddForInitialization
+     * method
+     */    
     public void readFromFile(TaskManager tm) throws ParseException {
-        
-    	String[] inputs = new String[9];
-        inputs[COMMAND_TYPE] = "add";
         
         if(textFile.exists()) {
            
@@ -128,31 +228,101 @@ public class FileStorage {
         		Scanner sc = new Scanner(textFile);
                 
                 while (sc.hasNextLine()) {
-                    
-                	inputs[TASK_ID] = sc.next();
-                    inputs[TASK_NAME] = sc.next();
-                    inputs[DATE_FROM] = sc.next();
-                    if(!inputs[DATE_FROM].equals(EMPTY_INPUT)) {
-                        inputs[DATE_FROM] += " " + sc.next();
+                	
+                	String[] inputs = new String[9];
+                	String[] tempStringArray = new String[8];
+                	
+                	tempStringArray = sc.nextLine().split("\\s*,\\s*");
+                	System.out.println(tempStringArray.length);
+                	
+                	for(int i=0; i<tempStringArray.length;i++) {
+                		
+                		System.out.println(tempStringArray[i]);
+                		
+                	}
+                	             	
+                	inputs[COMMAND_TYPE_INDEX] = "add";
+                	inputs[TASK_ID_INDEX] = tempStringArray[0];
+ 
+                	
+                	inputs[TASK_NAME_INDEX] = tempStringArray[1];
+                 
+                    if(!tempStringArray[2].equals(EMPTY_INPUT)) {
+                    	
+                    	inputs[DATE_FROM_INDEX] = tempStringArray[2];
                     
                     }
                     
-                    inputs[DATE_TO] = sc.next();
-                    if(!inputs[DATE_TO].equals(EMPTY_INPUT)) {
-                        inputs[DATE_TO] += " " + sc.next();
+                    else {
+                    	
+                    	inputs[DATE_FROM_INDEX] = null;
+                    	
+                    }
+                                       
+                    if(!tempStringArray[3].equals(EMPTY_INPUT)) {
+                        
+                    	inputs[DATE_TO_INDEX] = tempStringArray[3];
+                    
                     }
                     
-                    inputs[DEADLINE] = sc.next();
-                    if(!inputs[DEADLINE].equals(EMPTY_INPUT)) {
-                        inputs[DEADLINE] += " " + sc.next();
+                    else {
+                    	
+                    	inputs[DATE_TO_INDEX] = null;
+                    	
+                    }
+                    
+                    
+                    if(!tempStringArray[4].equals(EMPTY_INPUT)) {
+                        
+                    	inputs[DEADLINE_INDEX] = tempStringArray[4];
                     
                     }
                     
-                    inputs[LOCATION] = sc.next();
-                    inputs[DETAILS] = sc.next();
-                    inputs[PRIORITY] = sc.next();
+                    else {
+                    	
+                    	inputs[DEADLINE_INDEX] = null;
+                    	
+                    }
                     
+                    if(!tempStringArray[5].equals(EMPTY_INPUT)) {
+                        
+                    	inputs[LOCATION_INDEX] = tempStringArray[5];
+                    
+                    }
+                    
+                    else {
+                    	
+                    	inputs[LOCATION_INDEX] = null;
+                    	
+                    }
+
+                    if(!tempStringArray[6].equals(EMPTY_INPUT)) {
+                        
+                    	inputs[DETAILS_INDEX] = tempStringArray[6];
+                    
+                    }
+                    
+                    else {
+                    	
+                    	inputs[DETAILS_INDEX] = null;
+                    	
+                    }
+                    
+                    
+                    if(!tempStringArray[7].equals(EMPTY_INPUT)) {
+                        
+                    	inputs[PRIORITY_INDEX] = tempStringArray[7];
+                    	
+                    }
+                    
+                    else {
+                    	
+                    	inputs[PRIORITY_INDEX] = null;
+                    	
+                    }
+                  
                     tm.processAddForInitialization(inputs);       
+               
                 }
                 
                 sc.close();
