c7c814a566b0744d2143a6e446676d65d62e32db
comilao <vincentm1991@gmail.com>
2015-03-20 16:37:06 +0800

    append "Task" at the back of the original command, to differentiate from commands for shortcut and template

--- src/COMMAND_TYPE_TASK_MANAGER.java
+++ src/COMMAND_TYPE_TASK_MANAGER.java
@@ -1,3 +1,3 @@
 public enum COMMAND_TYPE_TASK_MANAGER {
-    add, edit, view, delete, init, undo, redo, invalid
+    addTask, editTask, viewTask, deleteTask, initTask, undoTask, redoTask, invalidTask
 }
