3d518e77e602bdc72b92f03e5cdb3229d35aef78
MunAw <jasontanma@gmail.com>
2015-03-20 16:48:26 +0800

    FIX: singleton pattern for centralized log. Refer to document for updates

--- src/CentralizedLog.java
+++ src/CentralizedLog.java
@@ -4,18 +4,26 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class CentralizedLog {
-	private static Logger logfile;
+	private Logger logfile;
+	private static CentralizedLog centralLog;
+	
+	private CentralizedLog() {
+		logfile = buildLogger();
+	}
 	
 	/**
-	 * This method gets the logger used for this system
+	 * This method initialize centralLog object and 
+	 * gets the logger used for this system
 	 * @return	log file that is used among the system
 	 */
-	public static Logger getLogger() {
-		if(logfile == null) {
-			logfile = buildLogger();
+	public static CentralizedLog getLogger() {
+		if(centralLog == null) {
+			centralLog = new CentralizedLog();
 		}
-		return logfile;
+		
+		return centralLog;
 	}
+	
 	
 	/**
 	 * This method build a log file under file name log.txt
@@ -34,5 +42,35 @@ public class CentralizedLog {
 			
 		}
 		return logfile;
+	}
+	
+	public void setLevel(Level newLvl) {
+		logfile.setLevel(newLvl);
+	}
+	
+	public void log(Level level, String msg) {
+		logfile.log(level, msg);
+	}
+	
+	public void severe(String msg) {
+		logfile.severe(msg);
+	}
+	public void info(String msg) {
+		logfile.info(msg);
+	}
+	public void warning(String msg) {
+		logfile.warning(msg);
+	}
+	public void config(String msg) {
+		logfile.config(msg);
+	}
+	public void fine(String msg) {
+		logfile.fine(msg);
+	}
+	public void finer(String msg) {
+		logfile.finer(msg);
+	}
+	public void finest(String msg) {
+		logfile.finest(msg);
 	}
 }
