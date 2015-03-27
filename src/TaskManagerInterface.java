364c9a286522140cf821339ae6a8750697119a4f
comilao <vincentm1991@gmail.com>
2015-03-27 11:31:34 +0800

    add processTM() and getTaskFromTID()

--- src/TaskManagerInterface.java
+++ src/TaskManagerInterface.java
@@ -2,6 +2,26 @@ import java.util.ArrayList;
 
 
 public interface TaskManagerInterface {
-    public void processAddForInitialization();
+    
+    /**
+     * This method adds tasks from file once the program starts before user entering commands
+     * If the task does not have an ID, call getNewTID()
+     * @param inputs  a parsed command received from FileStorage, with command type
+     *                being "addTask"
+     */
+    public void processInitialization(String[] inputs);
+    
+    
+    /**
+     * @param inputs  a parsed command received from SystemHandler
+     * @return        affected tasks in an ArrayList
+     */
     public ArrayList<Task> processTM(String[] inputs);
+    
+    
+    /**
+     * @param TID  an integer Task ID
+     * @return     a Task with that ID, if ID exists; else return null
+     */
+    public Task getTaskFromTID(int TID);
 }
