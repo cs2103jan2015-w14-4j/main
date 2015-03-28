d75371119fa60038dfd4872c60c2fd256298928f
comilao <vincentm1991@gmail.com>
2015-03-28 16:51:12 +0800

    Comparator<Task> for comparing location

---
+++ src/ComparatorLocation.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+
+public class ComparatorLocation implements Comparator<Task>{
+
+    public int compare(Task task1, Task task2) {
+        String location1 = task1.getLocation();
+        String location2 = task2.getLocation();
+        if(location1 != null && location2 != null) {
+            if(location1.compareTo(location2) == 0) {
+                if(task1.getTID() < task2.getTID()) {
+                    return -1;
+                } else if(task1.getTID() > task1.getTID()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return location1.compareTo(location2);
+            }
+        } else if(location1 == null && location2 != null) {
+            return 1;
+        } else if(location1 != null && location2 == null) {
+            return -1;
+        } else {
+            if(task1.getTID() < task2.getTID()) {
+                return -1;
+            } else if(task1.getTID() > task1.getTID()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
