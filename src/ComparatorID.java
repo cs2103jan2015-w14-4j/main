77c6853e251ecb62816bb44eeeef2f2584869df6
comilao <vincentm1991@gmail.com>
2015-03-28 16:49:37 +0800

    added Comparator<Task> for comparing ID

---
+++ src/ComparatorID.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class ComparatorID implements Comparator<Task> {
+
+    public int compare(Task task1, Task task2) {
+        if(task1.getTID() < task2.getTID()) {
+            return -1;
+        } else if(task1.getTID() > task1.getTID()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
