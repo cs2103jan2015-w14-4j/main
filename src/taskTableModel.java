e5bf5da2ed2271e45a3857fa9dedfc9af971f1b3
yhannq <yhann.q@gmail.com>
2015-03-30 23:32:45 +0800

    Updated to display String[] table

--- src/taskTableModel.java
+++ src/taskTableModel.java
@@ -1,27 +1,35 @@
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableColumn;
 
 
 public class TaskTableModel extends AbstractTableModel {
-	
+
 	/**
 	 * 
 	 */
 	private static ArrayList<Task> taskArray;
+	private static ArrayList<String[]>taskArrayString;
 	protected ArrayList<String> columnNames;
 	protected Class[] columnClasses;
 	protected Boolean[] isColumnEditable;
 	private Class rowClass = Object.class;
 	private boolean isModelEditable = true;
-	
-	
+
+	private List<Color> rowColours = Arrays.asList(
+			Color.GREEN,
+			Color.CYAN
+			);
+
+
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	/**
 	 *  Full Constructor for creating a <code>RowTableModel</code>.
 	 *
@@ -37,173 +45,170 @@ public class TaskTableModel extends AbstractTableModel {
 	 *						of the new columns
 	 *  @param rowClass     the class of row data to be added to the model
 	 */
-	
-	public TaskTableModel(ArrayList<Task> modelData, ArrayList<String> columnNames, Class rowClass)
+
+	public TaskTableModel(ArrayList<String[]> modelData, ArrayList<String> columnNames, Class rowClass)
 	{
 		setDataAndColumnNames(modelData, columnNames);
 		setRowClass( rowClass );
 	}
-	
-	protected void setDataAndColumnNames(ArrayList<Task> modelData, ArrayList<String> columnNames)
+
+	protected void setDataAndColumnNames(ArrayList<String[]> modelData, ArrayList<String> columnNames)
 	{
-		taskArray = modelData;
+		taskArrayString = modelData;
 		this.columnNames = columnNames;
 		columnClasses = new Class[getColumnCount()];
 		isColumnEditable = new Boolean[getColumnCount()];
 		fireTableStructureChanged();
 	}
-	
+
 	protected void setRowClass(Class rowClass)
 	{
 		this.rowClass = rowClass;
 	}
-	
+
+	/*
 	public void setColumnClass(int column, Class columnClass)
 	{
 		columnClasses[column] = columnClass;
 		fireTableRowsUpdated(0, getRowCount() - 1);
 	}
-	
-	  @Override
-      public int getRowCount() {
-          return taskArray.size();
-      }
+	*/
 
-      @Override
-  	public int getColumnCount()
-  	{
-  		return columnNames.size();
-  	}
-      
-  	public Class getColumnClass(int column)
-  	{
-  		Class columnClass = null;
-
-  		//  Get the class, if set, for the specified column
-
-  		if (column < columnClasses.length)
-  			columnClass = columnClasses[column];
-
-  		//  Get the default class
-
-  		if (columnClass == null)
-  			columnClass = super.getColumnClass(column);
-
-  		return columnClass;
-  	}
-/*
-      @Override
-      public Class<?> getColumnClass(int columnIndex) {
-          Class type = String.class;
-          switch (columnIndex) {
-              case 0://TID
-            	  type = Integer.class;
-            	  break;
-              case 1://TaskName
-            	  break;
-              case 2://dateFrom
-            	  type = Date.class;
-            	  break;
-              case 3://dateTo
-                  type = Date.class;
-                  break;
-              case 4://deadline
-            	  type = Date.class;
-                  break;
-              case 5://location
-            	  break;
-              case 6://details
-            	  break;
-              case 7://priority
-            	  type = Integer.class;
-            	  break;
-              		
-          }
-          return type;
-      }
-      */
-      
-      public String getColumnName(int column)
-  	{
-  		Object columnName = null;
-
-  		if (column < columnNames.size())
-  		{
-  			columnName = columnNames.get( column );
-  		}
-
-  		return (columnName == null) ? super.getColumnName( column ) : columnName.toString();
-  	}
-      
-  	public boolean isCellEditable(int row, int column)
-  	{
-  		Boolean isEditable = null;
-
-  		//  Check is column editability has been set
-
-  		if (column < isColumnEditable.length)
-  			isEditable = isColumnEditable[column];
-
-  		return (isEditable == null) ? isModelEditable : isEditable.booleanValue();
-  	}
-      
-
-
-      @Override
-      public Object getValueAt(int rowIndex, int columnIndex) {
-         Task task = taskArray.get(rowIndex);
-          Object value = null;
-          switch (columnIndex) {
-	          case 0:
-	              value = task.getTID();
-	              break;
-          	  case 1:
-                  value = task.getTaskName();
-                  break;
-              case 2:
-                  value = task.getDateFrom();
-                  break;
-              case 3:
-                  value = task.getDateTo();
-                  break;
-              case 4:
-                  value = task.getDeadline();
-                  break;
-              case 5:
-                  value = task.getLocation();
-                  break;
-              case 6:
-                  value = task.getDetails();
-                  break;
-              case 7:
-                  value = task.getPriority();
-                  break;
-          }
-          return value;
-      }  
-      
-  	/**
-  	 *  Adds a row of data to the end of the model.
-  	 *  Notification of the row being added will be generated.
-  	 *
-  	 * @param   rowData		 data of the row being added
-  	 */
-  	public void addRow(Task rowData)
-  	{
-  		insertRow(getRowCount(), rowData);
-  	}
-  	
-	public void insertRow(int row, Task rowData)
+	public Class getColumnClass(int column)
 	{
-		taskArray.add(row, rowData);
+		Class columnClass = null;
+
+		//  Get the class, if set, for the specified column
+
+		if (column < columnClasses.length)
+			columnClass = columnClasses[column];
+
+		//  Get the default class
+
+		if (columnClass == null)
+			columnClass = super.getColumnClass(column);
+
+		return columnClass;
+	}
+
+
+	public void setRowColour(int row, Color c) {
+		System.out.println("rowCOlours?" + rowColours);
+		rowColours.set(row, c);
+		fireTableRowsUpdated(row, row);
+	}
+	public Color getRowColour(int row) {
+		return rowColours.get(row);
+	}
+
+	@Override
+	public int getRowCount() {
+		return taskArrayString.size();
+	}
+
+	@Override
+	public int getColumnCount()
+	{
+		return columnNames.size();
+	}
+
+	/**
+	 * Returns the column name.
+	 *
+	 * @return a name for this column using the string value of the
+	 * appropriate member in <code>columnNames</code>. If
+	 * <code>columnNames</code> does not have an entry for this index
+	 * then the default name provided by the superclass is returned
+	 */
+	public String getColumnName(int column)
+	{
+		Object columnName = null;
+
+		if (column < columnNames.size())
+		{
+			columnName = columnNames.get( column );
+		}
+
+		return (columnName == null) ? super.getColumnName( column ) : columnName.toString();
+	}
+	
+
+	/**
+	 * Returns true regardless of parameter values.
+	 *
+	 * @param   row		   the row whose value is to be queried
+	 * @param   column		the column whose value is to be queried
+	 * @return				  true
+	 */
+	public boolean isCellEditable(int row, int column)
+	{
+		Boolean isEditable = null;
+
+		//  Check is column editability has been set
+
+		if (column < isColumnEditable.length)
+			isEditable = isColumnEditable[column];
+
+		return (isEditable == null) ? isModelEditable : isEditable.booleanValue();
+	}
+
+
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Task task = taskArray.get(rowIndex);
+		Object value = null;
+		switch (columnIndex) {
+		case 0:
+			value = task.getTID();
+			break;
+		case 1:
+			value = task.getTaskName();
+			break;
+		case 2:
+			value = task.getDateFrom();
+			break;
+		case 3:
+			value = task.getDateTo();
+			break;
+		case 4:
+			value = task.getDeadline();
+			break;
+		case 5:
+			value = task.getLocation();
+			break;
+		case 6:
+			value = task.getDetails();
+			break;
+		case 7:
+			value = task.getPriority();
+			break;
+		}
+		return value;
+	}  
+
+	/**
+	 *  Adds a row of data to the end of the model.
+	 *  Notification of the row being added will be generated.
+	 *
+	 * @param   rowData		 data of the row being added
+	 */
+	public void addRow(String[] rowData)
+	{
+		insertRow(getRowCount(), rowData);
+	}
+
+	public void insertRow(int row, String[] rowData)
+	{
+		taskArrayString.add(row, rowData);
 		fireTableRowsInserted(row, row);
 	}
-      
-	public Task getRow(int row)
+
+	public String[] getRow(int row)
 	{
-		return taskArray.get( row );
+		return taskArrayString.get( row );
 	}
-	
+
 	/**
 	 *  Insert multiple rows of data at the <code>row</code> location in the model.
 	 *  Notification of the row being added will be generated.
@@ -211,52 +216,60 @@ public class TaskTableModel extends AbstractTableModel {
 	 * @param   row	  row in the model where the data will be inserted
 	 * @param   rowList  each item in the list is a separate row of data
 	 */
-	public void insertRows(int row, ArrayList<Task> rowList)
+	public void insertRows(int row, ArrayList<String[]> rowList)
 	{	
-		taskArray.addAll(row, rowList);
+		taskArrayString.addAll(row, rowList);
+	/*	for (int i = 0; i < getRowCount(); i++){
+			int colour  = i%2;
+			System.out.println("colour = " + colour );
+			setRowColour(i, rowColours.get(colour));
+		}
+		*/
+		//setRowColour(row, Color.GRAY);
 		fireTableRowsInserted(row, row + rowList.size() - 1);
 	}
-	
+
 	public void removeRowRange(int start, int end)
 	{
-		taskArray.subList(start, end ).clear();
+		taskArrayString.subList(start, end ).clear();
 		fireTableDataChanged();
 	}
-	
+
 	public void removeRows(int... rows)
 	{
-		
+
 		for (int i = rows.length - 1; i >= 0; i--)
 		{
 			int row = rows[i];
-			taskArray.remove(row);
+			taskArrayString.remove(row);
 			fireTableRowsDeleted(row, row);
 		}
 	}
-	
-    public void clearTable()
-    {
-        removeRowRange(0 ,getRowCount());
-    }
+
+	public void clearTable()
+	{
+		removeRowRange(0 ,getRowCount());
+	}
 
 
-	
-	public void refreshTable(ArrayList<Task> tableData){
-		
-		ArrayList<Task> newData = new ArrayList<Task>(tableData);
-		
-		removeRowRange(0,getRowCount());
+
+	public void refreshTable(ArrayList<String[]> tableData){
+
+		ArrayList<String[]> newData = new ArrayList<String[]>(tableData);
+		clearTable();
 		insertRows(0, newData);
 
+
 	}
- 
-      /*
+
+
+	/*
       public void setValueAt(Object aValue, int rowIndex, int columnIndex){
-    	  
+
     	  taskArray.get(rowIndex).get(columnIndex) = aValue;
     	 fireTableCellUpdated(rowIndex, columnIndex); 
       }
-      */
-      
-  }     
+	 */
+
+}     
 
