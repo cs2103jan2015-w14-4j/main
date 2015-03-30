7d2fdb5c51f2df02a147e63c2fa6e8a58198046b
comilao <vincentm1991@gmail.com>
2015-03-31 01:13:03 +0800

    removed test cases related to reminder operation

--- src/TaskManagerTest.java
+++ src/TaskManagerTest.java
@@ -4,8 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -52,8 +54,6 @@ public class TaskManagerTest {
     private static final String COMMAND_ADD = "addTask";
     private static final String COMMAND_DELETE = "deleteTask";
     private static final String COMMAND_EDIT = "editTask";
-    private static final String COMMAND_ADD_REMINDER = "addReminder";
-    private static final String COMMAND_DELETE_REMINDER = "deleteReminder";
 
 
     private static final String DEFAULT_DATE_FORMAT = "dd/MM/yyyy HH:mm";
@@ -62,13 +62,23 @@ public class TaskManagerTest {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        SystemHandler handler = SystemHandler.getSystemHandler();
+        handler.initialize("testTaskManager.txt");
     }
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
     }
+    
+    @Before
+    public void setUp() throws Exception {
+    }
 
-
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    
 
     //--------------------testing initialization starts-------------------
     @Test
@@ -864,132 +874,6 @@ public class TaskManagerTest {
         Assert.assertEquals(myTaskManager.getRedoStack().size(), 0);
     }
     //--------------------testing undo and redo command ends--------------------
-
-
-    //--------------------testing add reminder starts---------------------------
-    @Test
-    public void testAddReminder() {
-        myTaskManager = new TaskManager();
-        myTaskManager.processTM(ADD_TASK_10);
-
-        ArrayList<Task> expectTasks = new ArrayList<Task>();
-        Task expectTask10 = new Task(10, "CS2103T Tutorial", convertToDateObject("18/03/2015 14:00"), 
-                convertToDateObject("18/03/2015 15:00"), null, "SOC", null, 1);
-        expectTasks.add(expectTask10);
-        Date reminder = convertToDateObject("13/03/2015 15:00");
-        expectTasks.get(0).addReminders(reminder);
-
-        String[] addReminderTask10 = {"addReminder", "10", null, "13/03/2015 15:00", 
-                null, null, null, null, null};
-
-        assertTaskArrayListEquals(myTaskManager.processTM(addReminderTask10), expectTasks);        
-
-        //only one reminder is allowed at this time
-        assertTaskArrayListEquals(myTaskManager.processTM(addReminderTask10), null);
-    }
-
-    @Test
-    public void testUndoAndRedoForAddReminder() {
-        myTaskManager = new TaskManager();
-        myTaskManager.processInitialization(ADD_TASK_10);
-
-        ArrayList<Task> expectTasks = new ArrayList<Task>();
-        Task expectTask10 = new Task(10, "CS2103T Tutorial", convertToDateObject("18/03/2015 14:00"), 
-                convertToDateObject("18/03/2015 15:00"), null, "SOC", null, 1);
-        expectTasks.add(expectTask10);
-        Date reminder = convertToDateObject("13/03/2015 15:00");
-        expectTasks.get(0).addReminders(reminder);
-
-        String[] addReminderTask10 = {"addReminder", "10", null, "13/03/2015 15:00", 
-                null, null, null, null, null};
-
-        assertTaskArrayListEquals(myTaskManager.processTM(addReminderTask10), expectTasks);        
-        Assert.assertEquals(myTaskManager.getUndoStack().peek()[COMMAND_TYPE], 
-                COMMAND_ADD_REMINDER);
-        Assert.assertEquals(myTaskManager.getUndoStack().size(), 1);
-        Assert.assertEquals(myTaskManager.getRedoStack().size(), 0);
-
-        expectTasks.get(0).deleteReminders(reminder);
-        assertTaskArrayListEquals(myTaskManager.processTM(UNDO_OPERATION), expectTasks);
-        Assert.assertEquals(myTaskManager.getRedoStack().peek()[COMMAND_TYPE], 
-                COMMAND_ADD_REMINDER);
-        Assert.assertEquals(myTaskManager.getUndoStack().size(), 0);
-        Assert.assertEquals(myTaskManager.getRedoStack().size(), 1);
-
-        expectTasks.get(0).addReminders(reminder);
-        assertTaskArrayListEquals(myTaskManager.processTM(REDO_OPERATION), expectTasks);
-        Assert.assertEquals(myTaskManager.getUndoStack().peek()[COMMAND_TYPE], 
-                COMMAND_ADD_REMINDER);
-        Assert.assertEquals(myTaskManager.getUndoStack().size(), 1);
-        Assert.assertEquals(myTaskManager.getRedoStack().size(), 0);
-    }
-    //--------------------testing add reminder ends-----------------------------
-
-
-    //--------------------testing delete reminder starts------------------------
-    @Test
-    public void testDeleteReminder() {
-        myTaskManager = new TaskManager();
-        myTaskManager.processTM(ADD_TASK_10);
-
-        String[] addReminderTask10 = {"addReminder", "10", null, "13/03/2015 15:00", 
-                null, null, null, null, null};
-        myTaskManager.processTM(addReminderTask10);
-        String[] deleteReminderTask10 = {"deleteReminder", "10", null, null, 
-                null, null, null, null, null};
-
-        ArrayList<Task> expectTasks = new ArrayList<Task>();
-        Task expectTask10 = new Task(10, "CS2103T Tutorial", convertToDateObject("18/03/2015 14:00"), 
-                convertToDateObject("18/03/2015 15:00"), null, "SOC", null, 1);
-        expectTasks.add(expectTask10);
-
-
-        assertTaskArrayListEquals(myTaskManager.processTM(deleteReminderTask10), expectTasks);        
-
-        //only one reminder is allowed at this time
-        assertTaskArrayListEquals(myTaskManager.processTM(deleteReminderTask10), null);
-    }
-
-    @Test
-    public void testUndoAndRedoForDeleteReminder() {
-        myTaskManager = new TaskManager();
-        myTaskManager.processInitialization(ADD_TASK_10);
-
-        String[] addReminderTask10 = {"addReminder", "10", null, "13/03/2015 15:00", 
-                null, null, null, null, null};
-        myTaskManager.processTM(addReminderTask10);
-        String[] deleteReminderTask10 = {"deleteReminder", "10", null, null, 
-                null, null, null, null, null};
-
-        ArrayList<Task> expectTasks = new ArrayList<Task>();
-        Task expectTask10 = new Task(10, "CS2103T Tutorial", convertToDateObject("18/03/2015 14:00"), 
-                convertToDateObject("18/03/2015 15:00"), null, "SOC", null, 1);
-        expectTasks.add(expectTask10);
-
-
-        assertTaskArrayListEquals(myTaskManager.processTM(deleteReminderTask10), expectTasks);      
-        Assert.assertEquals(myTaskManager.getUndoStack().peek()[COMMAND_TYPE], 
-                COMMAND_DELETE_REMINDER);
-        Assert.assertEquals(myTaskManager.getUndoStack().size(), 2);
-        Assert.assertEquals(myTaskManager.getRedoStack().size(), 0);
-        
-        
-        Date reminder = convertToDateObject("13/03/2015 15:00");
-        expectTasks.get(0).addReminders(reminder);
-        assertTaskArrayListEquals(myTaskManager.processTM(UNDO_OPERATION), expectTasks);
-        Assert.assertEquals(myTaskManager.getRedoStack().peek()[COMMAND_TYPE], 
-                COMMAND_DELETE_REMINDER);
-        Assert.assertEquals(myTaskManager.getUndoStack().size(), 1);
-        Assert.assertEquals(myTaskManager.getRedoStack().size(), 1);
-
-        expectTasks.get(0).deleteReminders(reminder);
-        assertTaskArrayListEquals(myTaskManager.processTM(REDO_OPERATION), expectTasks);
-        Assert.assertEquals(myTaskManager.getUndoStack().peek()[COMMAND_TYPE], 
-                COMMAND_DELETE_REMINDER);
-        Assert.assertEquals(myTaskManager.getUndoStack().size(), 2);
-        Assert.assertEquals(myTaskManager.getRedoStack().size(), 0);
-    }
-    //--------------------testing delete reminder ends--------------------------
 
 
     //--------------------testing others starts---------------------------------
