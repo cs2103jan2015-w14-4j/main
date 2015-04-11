0f1045248bffbf7e2017a89e2050db06c67c3589
weiquan <low.weiquan@gmail.com>
2015-04-11 12:28:29 +0800

    refactored comments

--- src/FlexiParser.java
+++ src/FlexiParser.java
@@ -14,12 +14,7 @@ import java.util.Map;
 import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
 
-
-
 //@author A0116514N
-//add Task behind all the command
-//add escape if n
-
 public class FlexiParser {
 	private static final String MSG_ERR_INTERNAL_PARSER_IMPLEMENTATION = "\"%s\" is not implemented in parser yet.";
 	private static final String MSG_ERR_UNRECOGNIZED_COMMAND = "\"%s\" is not a command recognized by Flexi Tracker. Type \"viewKeyword\" to see all the keywords.";
